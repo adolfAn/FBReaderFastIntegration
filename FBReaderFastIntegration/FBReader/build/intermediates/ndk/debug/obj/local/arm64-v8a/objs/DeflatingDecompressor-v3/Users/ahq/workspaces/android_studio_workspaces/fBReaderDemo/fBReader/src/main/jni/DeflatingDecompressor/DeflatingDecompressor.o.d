5777bb7c9444a2acf14cf99568cb955559d5758d
anhq <anhq@2345.com>
2016-05-17 17:07:59 +0800

    基于FBReader2.5.10快速集成项目。

---
+++ FBReaderFastIntegration/FBReader/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/DeflatingDecompressor-v3/Users/ahq/workspaces/android_studio_workspaces/fBReaderDemo/fBReader/src/main/jni/DeflatingDecompressor/DeflatingDecompressor.o.d
@@ -0,0 +1,35 @@
+/Users/ahq/workspaces/android_studio_workspaces/fBReaderDemo/fBReader/build/intermediates/ndk/debug/obj/local/arm64-v8a/objs/DeflatingDecompressor-v3//Users/ahq/workspaces/android_studio_workspaces/fBReaderDemo/fBReader/src/main/jni/DeflatingDecompressor/DeflatingDecompressor.o: \
+ /Users/ahq/workspaces/android_studio_workspaces/fBReaderDemo/fBReader/src/main/jni/DeflatingDecompressor/DeflatingDecompressor.cpp \
+ /Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/jni.h \
+ /Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h \
+ /Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h \
+ /Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/android/api-level.h \
+ /Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/string.h \
+ /Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/malloc.h \
+ /Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/xlocale.h \
+ /Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/zlib.h \
+ /Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/zconf.h \
+ /Users/ahq/workspaces/android_dev/android-ndk-r10e/sources/cxx-stl/system/include/new \
+ /Users/ahq/workspaces/android_dev/android-ndk-r10e/sources/cxx-stl/system/include/cstddef
+
+/Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/jni.h:
+
+/Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/cdefs.h:
+
+/Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/sys/cdefs_elf.h:
+
+/Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/android/api-level.h:
+
+/Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/string.h:
+
+/Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/malloc.h:
+
+/Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/xlocale.h:
+
+/Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/zlib.h:
+
+/Users/ahq/workspaces/android_dev/android-ndk-r10e/platforms/android-21/arch-arm64/usr/include/zconf.h:
+
+/Users/ahq/workspaces/android_dev/android-ndk-r10e/sources/cxx-stl/system/include/new:
+
+/Users/ahq/workspaces/android_dev/android-ndk-r10e/sources/cxx-stl/system/include/cstddef:
